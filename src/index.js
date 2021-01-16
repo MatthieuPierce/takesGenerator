8e0dd9ef8cce98513782b8bb14bbedd1b361a16d
Matthieu Pierce <matthieu.pierce@gmail.com>
2021-01-15 18:10:03 -0800

    spinning up redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+//Default imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Redux imports
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers'
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
